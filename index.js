1b3dad367aad452d2e437a0e881ade27d62b977d
Murilo Sérgio rodrigues Júnior <murilo.cc.uepb@gmail.com>
2020-09-17 11:05:06 -0300

    Create initiate functions in index.js

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+const { getCIDRinNetworkAddress,
+    getLastTwoOctetsInNetworkAddress,
+     } = require('./util-functions');
+
+const {
+    generateNewNATFirewallRule,
+    generateNewNatTCPFirewallRule,
+    generateNewNatUDPFirewallRule,
+    generateNewNatMasqueradeFirewallRule
+} = require('./generate-rules-functions');
+
+
+var version="";
+var outputStream;
+
+function getOutputStream(){
+    return outputStream;
+}
+
+function createOutputStream(){
+    outputStream="";
+    outputStream+=version;
+}
+
+function setVersion(){
+    let date = new Date();
+    version="Version "+date.toLocaleDateString()+"_"+date.toLocaleTimeString();
+}
+
+function appendOutputStream(command){
+    outputStream+="\n"+command;
+}
+
+
+setVersion();
+createOutputStream();
+
+
+
+
 
 
 
--- util-functions.js
+++ util-functions.js
@@ -1,10 +1,9 @@
 module.exports={
-    getCIDRinNetworkAddress: function getCIDRinNetworkAddress(networkAddress) {
+    getCIDRinNetworkAddress: (networkAddress)=> {
         return networkAddress.split("/")[1];        
     }
     ,
-    getLastTwoOctetsInNetworkAddress: function getTwoLastOctetsInNetworkAddress(networkAddress) {
-        
+    getLastTwoOctetsInNetworkAddress: (networkAddress)=> {
         return networkAddress.split("/")[0].split(".")[2] +"."+ networkAddress.split("/")[0].split(".")[3];
     }
 }
